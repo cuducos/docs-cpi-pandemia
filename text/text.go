8cc583039256b622b85f3371adc68843c20f5dca
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-07-26 13:43:58 -0400

    Normalize names to UTF-8

--- text/text.go
+++ text/text.go
@@ -2,7 +2,9 @@ package text
 
 import (
 	"unicode"
+	"unicode/utf8"
 
+	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/runes"
 	"golang.org/x/text/transform"
 	"golang.org/x/text/unicode/norm"
@@ -11,5 +13,15 @@ import (
 func Normalize(s string) (string, error) {
 	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
 	r, _, err := transform.String(t, s)
+	if err != nil {
+		return "", err
+	}
+
+	if !utf8.ValidString(r) {
+		r, err = charmap.ISO8859_1.NewDecoder().String(r)
+		if err != nil {
+			return "", err
+		}
+	}
 	return r, err
 }
